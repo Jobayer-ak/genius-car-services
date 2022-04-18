2a546a735d0c5239df3f3a53f05788d9d574503f
Jobayer-ak <jobayer.ak@gmail.com>
2022-04-18 21:38:50 +0600

    created services.json

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,12 @@
+import React from "react";
+import repair1 from "../../../images/repair-1.png";
+
+const Services = () => {
+  return (
+    <div>
+      <h2>Services: </h2>
+    </div>
+  );
+};
+
+export default Services;
