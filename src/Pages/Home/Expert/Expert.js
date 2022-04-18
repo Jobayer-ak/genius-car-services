9271e8782fe27dba4c1ce94e1eef2781fbdd75ee
Jobayer-ak <jobayer.ak@gmail.com>
2022-04-18 22:50:57 +0600

    added images

---
+++ src/Pages/Home/Expert/Expert.js
@@ -0,0 +1,22 @@
+import React from "react";
+import expert1 from "../../../images/experts/expert-1.jpg";
+import expert2 from "../../../images/experts/expert-2.jpg";
+import expert3 from "../../../images/experts/expert-3.jpg";
+import expert4 from "../../../images/experts/expert-4.jpg";
+import expert5 from "../../../images/experts/expert-5.jpg";
+import expert6 from "../../../images/experts/expert-6.jpg";
+
+const experts = [
+  { id: 1, name: "Will Smith", img: expert1 },
+  { id: 1, name: "Chris Rock", img: expert2 },
+  { id: 1, name: "Dwayne Rock", img: expert3 },
+  { id: 1, name: "Nissam Liam", img: expert4 },
+  { id: 1, name: "Stachy Jhonson", img: expert5 },
+  { id: 1, name: "Conrad Hawkins", img: expert6 },
+];
+
+const Expert = () => {
+  return <div></div>;
+};
+
+export default Expert;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,10 +1,25 @@
-import React from "react";
-import repair1 from "../../../images/repair-1.png";
+import React, { useEffect, useState } from "react";
+import Service from "../Service/Service";
+import "./Services.css";
+// import repair1 from "../../../images/repair-1.png";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
   return (
     <div>
-      <h2>Services: </h2>
+      <h1 className="services-title">Our Services</h1>
+      <div className="services-container">
+        {services.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </div>
     </div>
   );
 };
