1e270a2b3c6d01ecfb42556d1c955779dee458ae
Jobayer-ak <jobayer.ak@gmail.com>
2022-04-19 13:11:23 +0600

    hk

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import "./Register.css";
+import auth from "../../../firebase.init";
 
 const Register = () => {
+  //   const [createUserWithEmailAndPassword, user, loading, error] =
+  //     useCreateUserWithEmailAndPassword(auth);
+
   const navigate = useNavigate();
 
   const navigateLogin = () => {
@@ -14,8 +19,8 @@ const Register = () => {
     const name = event.target.name.value;
     const email = event.target.email.value;
     const password = event.target.password.value;
-    // console.log(event.target.email.value);
-    // console.log(event.target.password.value);
+
+    // createUserWithEmailAndPassword(email, password);
   };
 
   return (
